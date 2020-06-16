0d14a8eaedda16789821c39a6656e77793b8a91b
Mlie <emipa606@users.noreply.github.com>
2020-06-16 23:54:14 +0200

    First publish

---
+++ Source/ImpassableFurniture/ImpassableSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace ImpassableFurniture
+{
+	// Token: 0x02000002 RID: 2
+	internal class ImpassableSettings : ModSettings
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			List<ThingDef> list = this.enabledDefList;
+			List<string> list2;
+			if (list == null)
+			{
+				list2 = null;
+			}
+			else
+			{
+				list2 = (from td in list
+				select td.defName).ToList<string>();
+			}
+			List<string> source = list2 ?? new List<string>();
+			Scribe_Collections.Look<string>(ref source, "enabledDefList", LookMode.Undefined, new object[0]);
+			this.enabledDefList = (from td in source.Select(new Func<string, ThingDef>(DefDatabase<ThingDef>.GetNamedSilentFail))
+			where td != null
+			select td).ToList<ThingDef>();
+		}
+
+		// Token: 0x04000001 RID: 1
+		public List<ThingDef> enabledDefList = new List<ThingDef>();
+	}
+}
