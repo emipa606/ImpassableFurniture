bdd67c2d065dfbb561ca55174951e210a2d840d4
emipa606 <emil.offermo@litet.net>
2021-07-09 22:10:37 +0200

    Mod updated for 1.3 and passed autotests

--- Source/ImpassableFurniture/ImpassableFurniture.cs
+++ Source/ImpassableFurniture/ImpassableFurniture.cs
@@ -1,85 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Verse;
 
 namespace ImpassableFurniture
 {
-	// Token: 0x02000004 RID: 4
-	[StaticConstructorOnStartup]
-	internal static class ImpassableFurniture
-	{
-		// Token: 0x0600000A RID: 10 RVA: 0x00002678 File Offset: 0x00000878
-		static ImpassableFurniture()
-		{
+    // Token: 0x02000004 RID: 4
+    [StaticConstructorOnStartup]
+    internal static class ImpassableFurniture
+    {
+        // Token: 0x0600000A RID: 10 RVA: 0x00002678 File Offset: 0x00000878
+        static ImpassableFurniture()
+        {
             ImpassableFurnitureMod.instance.defaultList = (from td in DefDatabase<ThingDef>.AllDefsListForReading
-                                                           where td.building != null && td.passability == Traversability.Impassable
-                                                           select td).ToList<ThingDef>();
+                where td.building != null && td.passability == Traversability.Impassable
+                select td).ToList();
 
-            if (ImpassableFurnitureMod.instance.Settings.enabledDefList.NullOrEmpty<ThingDef>())
-			{
-				ImpassableFurnitureMod.instance.Settings.enabledDefList = (from td in DefDatabase<ThingDef>.AllDefsListForReading
-				where td.building != null && td.passability == Traversability.Impassable
-				select td).ToList<ThingDef>();
-				return;
-			}
-			DefDatabase<ThingDef>.AllDefsListForReading.ForEach(delegate(ThingDef td)
-			{
-				if (td.building != null && td.passability == Traversability.Impassable)
-				{
-					ImpassableFurniture.RemovePassability(td);
-				}
-			});
-			ImpassableFurnitureMod.instance.Settings.enabledDefList.ForEach(new Action<ThingDef>(ImpassableFurniture.AddPassability));
-		}
+            if (ImpassableFurnitureMod.instance.Settings.enabledDefList.NullOrEmpty())
+            {
+                ImpassableFurnitureMod.instance.Settings.enabledDefList =
+                    (from td in DefDatabase<ThingDef>.AllDefsListForReading
+                        where td.building != null && td.passability == Traversability.Impassable
+                        select td).ToList();
+                return;
+            }
 
-		// Token: 0x0600000B RID: 11 RVA: 0x00002704 File Offset: 0x00000904
-		public static void AddPassability(ThingDef def)
-		{
-			def.passability = Traversability.Impassable;
-			ImpassableFurniture.Regenerate();
-		}
+            DefDatabase<ThingDef>.AllDefsListForReading.ForEach(delegate(ThingDef td)
+            {
+                if (td.building != null && td.passability == Traversability.Impassable)
+                {
+                    RemovePassability(td);
+                }
+            });
+            ImpassableFurnitureMod.instance.Settings.enabledDefList.ForEach(AddPassability);
+        }
+
+        // Token: 0x0600000B RID: 11 RVA: 0x00002704 File Offset: 0x00000904
+        public static void AddPassability(ThingDef def)
+        {
+            def.passability = Traversability.Impassable;
+            Regenerate();
+        }
 
         public static void AddPassability(List<ThingDef> defList)
         {
-            foreach(ThingDef def in defList)
+            foreach (var def in defList)
+            {
                 def.passability = Traversability.Impassable;
-            ImpassableFurniture.Regenerate();
+            }
+
+            Regenerate();
         }
 
         // Token: 0x0600000C RID: 12 RVA: 0x00002712 File Offset: 0x00000912
         public static void RemovePassability(ThingDef def)
-		{
-			if (def.passability != Traversability.Impassable)
-			{
-				return;
-			}
-			def.passability = Traversability.PassThroughOnly;
-			ImpassableFurniture.Regenerate();
-		}
+        {
+            if (def.passability != Traversability.Impassable)
+            {
+                return;
+            }
+
+            def.passability = Traversability.PassThroughOnly;
+            Regenerate();
+        }
 
         public static void RemovePassability(List<ThingDef> defList)
         {
-            foreach (ThingDef def in defList)
+            foreach (var def in defList)
             {
-                if (def.passability != Traversability.Impassable) continue;
+                if (def.passability != Traversability.Impassable)
+                {
+                    continue;
+                }
+
                 def.passability = Traversability.PassThroughOnly;
             }
-            ImpassableFurniture.Regenerate();
+
+            Regenerate();
         }
 
         // Token: 0x0600000D RID: 13 RVA: 0x0000272C File Offset: 0x0000092C
         private static void Regenerate()
-		{
-			if (Current.ProgramState != ProgramState.Playing)
-			{
-				return;
-			}
-			foreach (Map map in Find.Maps)
-			{
-				map.pathGrid.RecalculateAllPerceivedPathCosts();
-				map.regionAndRoomUpdater.RebuildAllRegionsAndRooms();
-			}
-		}
-	}
+        {
+            if (Current.ProgramState != ProgramState.Playing)
+            {
+                return;
+            }
+
+            foreach (var map in Find.Maps)
+            {
+                map.pathing.RecalculateAllPerceivedPathCosts();
+                map.regionAndRoomUpdater.RebuildAllRegionsAndRooms();
+            }
+        }
+    }
 }
--- Source/ImpassableFurniture/ImpassableFurnitureMod.cs
+++ Source/ImpassableFurniture/ImpassableFurnitureMod.cs
@@ -1,177 +1,198 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Verse;
 
 namespace ImpassableFurniture
 {
-	// Token: 0x02000003 RID: 3
-	[StaticConstructorOnStartup]
-	internal class ImpassableFurnitureMod : Mod
-	{
-		// Token: 0x06000003 RID: 3 RVA: 0x0000210A File Offset: 0x0000030A
-		public ImpassableFurnitureMod(ModContentPack content) : base(content)
-		{
-			ImpassableFurnitureMod.instance = this;
-		}
-
-		// Token: 0x17000001 RID: 1
-		// (get) Token: 0x06000004 RID: 4 RVA: 0x00002124 File Offset: 0x00000324
-		// (set) Token: 0x06000005 RID: 5 RVA: 0x0000214A File Offset: 0x0000034A
-		internal ImpassableSettings Settings
-		{
-			get
-			{
-				ImpassableSettings result;
-				if ((result = this.settings) == null)
-				{
-					result = (this.settings = base.GetSettings<ImpassableSettings>());
-				}
-				return result;
-			}
-			set
-			{
-				this.settings = value;
-			}
-		}
-
-		// Token: 0x06000006 RID: 6 RVA: 0x00002154 File Offset: 0x00000354
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
-			base.DoSettingsWindowContents(inRect);
-			Text.Font = GameFont.Medium;
-			Rect rect = GenUI.TopPart(inRect, 0.05f);
-            this.searchTerm = Widgets.TextField(GenUI.LeftPart(GenUI.RightPart(rect, 0.95f), 0.95f), this.searchTerm);
-			Rect rect2 = GenUI.BottomHalf(GenUI.TopPart(inRect, 0.1f));
-			Rect rect3 = GenUI.BottomPart(inRect, 0.9f);
-			Rect rect4 = GenUI.LeftPart(GenUI.RightPart(GenUI.LeftHalf(rect3), 0.9f), 0.9f);
-			GUI.BeginGroup(rect4, new GUIStyle(GUI.skin.box));
-			List<ThingDef> list = (from td in DefDatabase<ThingDef>.AllDefs
-			where td.building != null && !td.label.Contains("(building)") && td.passability != Traversability.Impassable && (td.defName.Contains(this.searchTerm) || td.label.Contains(this.searchTerm)) && !this.Settings.enabledDefList.Contains(td)
-			orderby td.LabelCap.RawText ?? td.defName
-			select td).ToList<ThingDef>();
-			float num = 3f;
-			Widgets.BeginScrollView(GenUI.AtZero(rect4), ref this.leftScrollPosition, new Rect(0f, 0f, rect4.width / 10f * 9f, (float)list.Count * 32f), true);
-			if (!list.NullOrEmpty<ThingDef>())
-			{
-				foreach (ThingDef thingDef in list)
-				{
-					Rect rect5 = new Rect(5f, num, rect4.width - 6f, 30f);
-					Widgets.DrawHighlightIfMouseover(rect5);
-					if (thingDef == this.leftSelectedDef)
-					{
-						Widgets.DrawHighlightSelected(rect5);
-					}
-					Widgets.Label(rect5, thingDef.LabelCap.RawText ?? thingDef.defName);
-					if (Widgets.ButtonInvisible(rect5, false))
-					{
-						this.leftSelectedDef = thingDef;
-					}
-					num += 32f;
-				}
-			}
-			Widgets.EndScrollView();
-			GUI.EndGroup();
-            Widgets.Label(GenUI.RightPart(GenUI.LeftHalf(rect2), 0.9f), "Green: All, Yellow: Default, Red: None");
-			Widgets.Label(GenUI.RightPart(GenUI.RightHalf(rect2), 0.9f), "Enable Impassability for:");
-			Rect rect6 = GenUI.LeftPart(GenUI.RightPart(GenUI.RightHalf(rect3), 0.9f), 0.9f);
-			GUI.BeginGroup(rect6, GUI.skin.box);
-			num = 6f;
-			Widgets.BeginScrollView(GenUI.AtZero(rect6), ref this.rightScrollPosition, new Rect(0f, 0f, rect6.width / 5f * 4f, (float)this.Settings.enabledDefList.Count * 32f), true);
-			if (!this.Settings.enabledDefList.NullOrEmpty<ThingDef>())
-			{
-				foreach (ThingDef thingDef2 in from def in this.Settings.enabledDefList
-				where def.defName.Contains(this.searchTerm) || def.label.Contains(this.searchTerm)
-				select def)
-				{
-					Rect rect7 = new Rect(5f, num, rect4.width - 6f, 30f);
-					Widgets.DrawHighlightIfMouseover(rect7);
-					if (thingDef2 == this.rightSelectedDef)
-					{
-						Widgets.DrawHighlightSelected(rect7);
-					}
-					Widgets.Label(rect7, thingDef2.LabelCap.RawText ?? thingDef2.defName);
-					if (Widgets.ButtonInvisible(rect7, false))
-					{
-						this.rightSelectedDef = thingDef2;
-					}
-					num += 32f;
-				}
-			}
-			Widgets.EndScrollView();
-			GUI.EndGroup();
-            if (Widgets.ButtonImage(GenUI.LeftPart(GenUI.RightPart(GenUI.TopPart(GenUI.BottomPart(rect3, 0.7f), 0.08f), 0.525f), 0.1f), TexUI.ArrowTexRight, Color.green))
-            {
-                this.leftSelectedDef = null;
-                ImpassableFurniture.AddPassability(list);
-                foreach (ThingDef def in list)
-                    this.Settings.enabledDefList.Add(def);
-            }
-            if (Widgets.ButtonImage(GenUI.LeftPart(GenUI.RightPart(GenUI.TopPart(GenUI.BottomPart(rect3, 0.6f), 0.1f), 0.525f), 0.1f), TexUI.ArrowTexRight) && this.leftSelectedDef != null)
-			{
-				this.Settings.enabledDefList.Add(this.leftSelectedDef);
-				this.Settings.enabledDefList = (from td in this.Settings.enabledDefList
-				orderby td.LabelCap.RawText ?? td.defName
-				select td).ToList<ThingDef>();
-				this.rightSelectedDef = this.leftSelectedDef;
-				this.leftSelectedDef = null;
-				ImpassableFurniture.AddPassability(this.rightSelectedDef);
-            }
-            if (Widgets.ButtonImage(GenUI.LeftPart(GenUI.RightPart(GenUI.TopPart(GenUI.BottomPart(rect3, 0.5f), 0.12f), 0.525f), 0.1f), TexUI.ArrowTex, Color.yellow))
-            {
-                this.leftSelectedDef = null;
-                this.rightSelectedDef = null;
-                ImpassableFurniture.RemovePassability(this.Settings.enabledDefList);
-                ImpassableFurniture.AddPassability(defaultList);
-                this.Settings.enabledDefList = new List<ThingDef>();
-                foreach (ThingDef def in defaultList)
-                    this.Settings.enabledDefList.Add(def);
-            }
-            if (Widgets.ButtonImage(GenUI.LeftPart(GenUI.RightPart(GenUI.TopPart(GenUI.BottomPart(rect3, 0.4f), 0.15f), 0.525f), 0.1f), TexUI.ArrowTexLeft) && this.rightSelectedDef != null)
-			{
-				this.Settings.enabledDefList.Remove(this.rightSelectedDef);
-				this.leftSelectedDef = this.rightSelectedDef;
-				this.rightSelectedDef = null;
-				ImpassableFurniture.RemovePassability(this.leftSelectedDef);
-            }
-            if (Widgets.ButtonImage(GenUI.LeftPart(GenUI.RightPart(GenUI.TopPart(GenUI.BottomPart(rect3, 0.3f), 0.17f), 0.525f), 0.1f), TexUI.ArrowTexLeft, Color.red))
-            {
-                this.rightSelectedDef = null;
-                ImpassableFurniture.RemovePassability(this.Settings.enabledDefList);
-                this.Settings.enabledDefList = new List<ThingDef>();
-            }
-            this.Settings.Write();
-		}
-
-		// Token: 0x06000007 RID: 7 RVA: 0x000025DC File Offset: 0x000007DC
-		public override string SettingsCategory()
-		{
-			return "Impassable Furniture";
-		}
-
-		// Token: 0x04000002 RID: 2
-		public static ImpassableFurnitureMod instance;
-
-		// Token: 0x04000003 RID: 3
-		private ImpassableSettings settings;
-
-		// Token: 0x04000004 RID: 4
-		private Vector2 leftScrollPosition;
-
-		// Token: 0x04000005 RID: 5
-		private Vector2 rightScrollPosition;
-
-		// Token: 0x04000006 RID: 6
-		private string searchTerm = "";
-
-		// Token: 0x04000007 RID: 7
-		private ThingDef leftSelectedDef;
-
-		// Token: 0x04000008 RID: 8
-		private ThingDef rightSelectedDef;
+    // Token: 0x02000003 RID: 3
+    [StaticConstructorOnStartup]
+    internal class ImpassableFurnitureMod : Mod
+    {
+        // Token: 0x04000002 RID: 2
+        public static ImpassableFurnitureMod instance;
 
         internal List<ThingDef> defaultList;
-        
-	}
+
+        // Token: 0x04000004 RID: 4
+        private Vector2 leftScrollPosition;
+
+        // Token: 0x04000007 RID: 7
+        private ThingDef leftSelectedDef;
+
+        // Token: 0x04000005 RID: 5
+        private Vector2 rightScrollPosition;
+
+        // Token: 0x04000008 RID: 8
+        private ThingDef rightSelectedDef;
+
+        // Token: 0x04000006 RID: 6
+        private string searchTerm = "";
+
+        // Token: 0x04000003 RID: 3
+        private ImpassableSettings settings;
+
+        // Token: 0x06000003 RID: 3 RVA: 0x0000210A File Offset: 0x0000030A
+        public ImpassableFurnitureMod(ModContentPack content) : base(content)
+        {
+            instance = this;
+        }
+
+        // Token: 0x17000001 RID: 1
+        // (get) Token: 0x06000004 RID: 4 RVA: 0x00002124 File Offset: 0x00000324
+        // (set) Token: 0x06000005 RID: 5 RVA: 0x0000214A File Offset: 0x0000034A
+        internal ImpassableSettings Settings
+        {
+            get
+            {
+                ImpassableSettings result;
+                if ((result = settings) == null)
+                {
+                    result = settings = GetSettings<ImpassableSettings>();
+                }
+
+                return result;
+            }
+            set => settings = value;
+        }
+
+        // Token: 0x06000006 RID: 6 RVA: 0x00002154 File Offset: 0x00000354
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            Text.Font = GameFont.Medium;
+            var rect = inRect.TopPart(0.05f);
+            searchTerm = Widgets.TextField(rect.RightPart(0.95f).LeftPart(0.95f), searchTerm);
+            var rect2 = inRect.TopPart(0.1f).BottomHalf();
+            var rect3 = inRect.BottomPart(0.9f);
+            var rect4 = rect3.LeftHalf().RightPart(0.9f).LeftPart(0.9f);
+            GUI.BeginGroup(rect4, new GUIStyle(GUI.skin.box));
+            var list = (from td in DefDatabase<ThingDef>.AllDefs
+                where td.building != null && !td.label.Contains("(building)") &&
+                      td.passability != Traversability.Impassable &&
+                      (td.defName.Contains(searchTerm) || td.label.Contains(searchTerm)) &&
+                      !Settings.enabledDefList.Contains(td)
+                orderby td.LabelCap.RawText ?? td.defName
+                select td).ToList();
+            var num = 3f;
+            Widgets.BeginScrollView(rect4.AtZero(), ref leftScrollPosition,
+                new Rect(0f, 0f, rect4.width / 10f * 9f, list.Count * 32f));
+            if (!list.NullOrEmpty())
+            {
+                foreach (var thingDef in list)
+                {
+                    var rect5 = new Rect(5f, num, rect4.width - 6f, 30f);
+                    Widgets.DrawHighlightIfMouseover(rect5);
+                    if (thingDef == leftSelectedDef)
+                    {
+                        Widgets.DrawHighlightSelected(rect5);
+                    }
+
+                    Widgets.Label(rect5, thingDef.LabelCap.RawText ?? thingDef.defName);
+                    if (Widgets.ButtonInvisible(rect5, false))
+                    {
+                        leftSelectedDef = thingDef;
+                    }
+
+                    num += 32f;
+                }
+            }
+
+            Widgets.EndScrollView();
+            GUI.EndGroup();
+            Widgets.Label(rect2.LeftHalf().RightPart(0.9f), "Green: All, Yellow: Default, Red: None");
+            Widgets.Label(rect2.RightHalf().RightPart(0.9f), "Enable Impassability for:");
+            var rect6 = rect3.RightHalf().RightPart(0.9f).LeftPart(0.9f);
+            GUI.BeginGroup(rect6, GUI.skin.box);
+            num = 6f;
+            Widgets.BeginScrollView(rect6.AtZero(), ref rightScrollPosition,
+                new Rect(0f, 0f, rect6.width / 5f * 4f, Settings.enabledDefList.Count * 32f));
+            if (!Settings.enabledDefList.NullOrEmpty())
+            {
+                foreach (var thingDef2 in from def in Settings.enabledDefList
+                    where def.defName.Contains(searchTerm) || def.label.Contains(searchTerm)
+                    select def)
+                {
+                    var rect7 = new Rect(5f, num, rect4.width - 6f, 30f);
+                    Widgets.DrawHighlightIfMouseover(rect7);
+                    if (thingDef2 == rightSelectedDef)
+                    {
+                        Widgets.DrawHighlightSelected(rect7);
+                    }
+
+                    Widgets.Label(rect7, thingDef2.LabelCap.RawText ?? thingDef2.defName);
+                    if (Widgets.ButtonInvisible(rect7, false))
+                    {
+                        rightSelectedDef = thingDef2;
+                    }
+
+                    num += 32f;
+                }
+            }
+
+            Widgets.EndScrollView();
+            GUI.EndGroup();
+            if (Widgets.ButtonImage(rect3.BottomPart(0.7f).TopPart(0.08f).RightPart(0.525f).LeftPart(0.1f),
+                TexUI.ArrowTexRight, Color.green))
+            {
+                leftSelectedDef = null;
+                ImpassableFurniture.AddPassability(list);
+                foreach (var def in list)
+                {
+                    Settings.enabledDefList.Add(def);
+                }
+            }
+
+            if (Widgets.ButtonImage(rect3.BottomPart(0.6f).TopPart(0.1f).RightPart(0.525f).LeftPart(0.1f),
+                TexUI.ArrowTexRight) && leftSelectedDef != null)
+            {
+                Settings.enabledDefList.Add(leftSelectedDef);
+                Settings.enabledDefList = (from td in Settings.enabledDefList
+                    orderby td.LabelCap.RawText ?? td.defName
+                    select td).ToList();
+                rightSelectedDef = leftSelectedDef;
+                leftSelectedDef = null;
+                ImpassableFurniture.AddPassability(rightSelectedDef);
+            }
+
+            if (Widgets.ButtonImage(rect3.BottomPart(0.5f).TopPart(0.12f).RightPart(0.525f).LeftPart(0.1f),
+                TexUI.ArrowTex, Color.yellow))
+            {
+                leftSelectedDef = null;
+                rightSelectedDef = null;
+                ImpassableFurniture.RemovePassability(Settings.enabledDefList);
+                ImpassableFurniture.AddPassability(defaultList);
+                Settings.enabledDefList = new List<ThingDef>();
+                foreach (var def in defaultList)
+                {
+                    Settings.enabledDefList.Add(def);
+                }
+            }
+
+            if (Widgets.ButtonImage(rect3.BottomPart(0.4f).TopPart(0.15f).RightPart(0.525f).LeftPart(0.1f),
+                TexUI.ArrowTexLeft) && rightSelectedDef != null)
+            {
+                Settings.enabledDefList.Remove(rightSelectedDef);
+                leftSelectedDef = rightSelectedDef;
+                rightSelectedDef = null;
+                ImpassableFurniture.RemovePassability(leftSelectedDef);
+            }
+
+            if (Widgets.ButtonImage(rect3.BottomPart(0.3f).TopPart(0.17f).RightPart(0.525f).LeftPart(0.1f),
+                TexUI.ArrowTexLeft, Color.red))
+            {
+                rightSelectedDef = null;
+                ImpassableFurniture.RemovePassability(Settings.enabledDefList);
+                Settings.enabledDefList = new List<ThingDef>();
+            }
+
+            Settings.Write();
+        }
+
+        // Token: 0x06000007 RID: 7 RVA: 0x000025DC File Offset: 0x000007DC
+        public override string SettingsCategory()
+        {
+            return "Impassable Furniture";
+        }
+    }
 }
